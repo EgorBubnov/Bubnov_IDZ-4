01e029385aaee65a3d97e50e5c4fadc77d0a4d16
EgorBubnov <Egorabubnov@gmail.com>
2024-11-30 10:54:32 +1000

    Add files of first IDZ

---
+++ 4a1.pas
@@ -0,0 +1,44 @@
+﻿program Pomogite;
+
+
+
+var
+  x, E: Real;
+  sum, term: Real;
+  i: Integer;
+
+function Factorial(n: Integer): Integer;
+var
+  j: Integer;
+begin
+  Result := 1;
+  for j := 2 to n do
+    Result := Result * j;
+end;
+
+//function DoubleFactorial(n: Integer): Integer;
+//var
+  //j: Integer;
+//begin
+  //Result := 1;
+  //for j := 1 to n do
+    //if j mod 2 = 0 then
+      //Result := Result * j;
+//end;
+
+begin
+  Write('Введите вещественное число x: ');
+  ReadLn(x);
+  Write('Введите точность E (0 < E << 1): ');
+  ReadLn(E);
+
+  sum := 0;
+  i := 1;
+  repeat
+    term := (Factorial(2 * i) + Abs(x)) / Factorial(i * i);
+    sum := sum + term;
+    i := i + 1;
+  until (term < E);
+
+  WriteLn('Результат суммы: ', sum);
+end.
---
+++ 4b1.pas
@@ -0,0 +1,48 @@
+﻿program ShtoIPishu;
+var
+  n, i, j: integer;   
+  wins, draws, losses: array[1..10] of integer; 
+  moreWinsThanLosses: integer; 
+  BolshePolovin: boolean; 
+begin
+  moreWinsThanLosses := 0;
+  BolshePolovin:= false; 
+
+  writeln('Введите количество команд (от 2 до 10): ');
+  read(n);
+  if (n<2) or (n>10) then
+  begin
+    writeln('Количество команд должно быть от 2 до 10');
+    exit;
+  end;
+
+  writeln('Введите данные о победах, ничьих и поражениях:');
+  for i := 1 to n do
+  begin
+    writeln('Команда ', i);
+
+    readln(wins[i], draws[i], losses[i]);
+
+    if wins[i] > losses[i] then
+      moreWinsThanLosses := moreWinsThanLosses + 1;
+
+    if wins[i] > draws[i] + losses[i] then
+      BolshePolovin := true;
+  end;
+  
+  writeln('Количество команд с больше победами, чем поражениями: ', moreWinsThanLosses);
+  
+  writeln('Команды без поражений: ');
+  for i := 1 to n do
+  begin
+    if losses[i] = 0 then
+      write(i, ' '); 
+  end;
+  
+  writeln; 
+
+  if BolshePolovin then
+    writeln('Существует команда, выигравшая более половины игр.')
+  else
+    writeln('Нет команд, выигравших более половины игр.');
+end.
